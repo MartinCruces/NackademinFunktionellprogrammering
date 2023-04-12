7a4b6538576da5bf73d4a9fa110384b8df582a73
Martin Cruces <martin76e@yahoo.com>
2023-04-12 19:11:26 +0200

    refactoring

--- src/AdventOfCode/2019Day1TheTyrannyOfRocketEquation.kt
+++ src/AdventOfCode/2019Day1TheTyrannyOfRocketEquation.kt
@@ -6,7 +6,7 @@ import java.io.File
 const val pathFile = "src/AdventOfCode/massList.txt"
 
 /* bränsle för moduler beräknad på dess massa (mass/3 -2) del 1
-   bänsle för muduler plus för bränslet själv del 2*/
+   bränsle för moduler plus för bränslet själv del 2*/
 
 
 //Läser in textfil med massan för alla moduler och mappar till en lista av int.
@@ -32,16 +32,20 @@ private fun readTextFileToIntList(fileName: String): List<Int>
     return fuel
 }
 
-//funktion med rekursion som tar in massan och beräknar förts bränslet för massan och sedan för varje varv bränsle för bränslet så länge bränslet är lika eller mindre än noll, då returnerar den totalsumman.
+//funktion med rekursion som tar in massan och beräknar först bränslet för massan och sedan för varje varv bränsle för bränslet så länge bränslet är lika eller mindre än noll, då returnerar den totalsumman.
 fun calFuelForFuelMass(mass: Int): Int {
     var fuel = mass/3 -2
     return if (fuel <= 0) 0
     else fuel + calFuelForFuelMass(fuel)
 
 }
-fun Int.fuel() :Int = this/3 - 2 //Använder en extention funktion för att beräkna bränslet
 
-fun Int.fuelForFuelMass(): Int =   // rekursion extention function. Mycket smidigare (Ginsberg)
+
+
+
+fun Int.fuel() :Int = this/3 - 2 //Använder en extension funktion för att beräkna bränslet
+
+fun Int.fuelForFuelMass(): Int =   // rekursion extension function. Mycket smidigare (Ginsberg)
     if(this < 9)  {
         0
     } else {
@@ -50,8 +54,8 @@ fun Int.fuelForFuelMass(): Int =   // rekursion extention function. Mycket smidi
     }
 fun main(){
 
-    println(readTextFileToIntList(pathFile).sumOf { it.fuel() }) //Beräknar bränslet för alla värden i listan på ett mycket smidigare sätt och anropar extention funktion fuel
-    println(readTextFileToIntList(pathFile).sumOf { it.fuelForFuelMass() }) //Beräknar även bränsle för bränslets massa med rekursiv extention funktion.
+    println(readTextFileToIntList(pathFile).sumOf { it.fuel() }) //Beräknar bränslet för alla värden i listan på ett mycket smidigare sätt och anropar extension funktion fuel
+    println(readTextFileToIntList(pathFile).sumOf { it.fuelForFuelMass() }) //Beräknar även bränsle för bränslets massa med rekursiv extension funktion.
 
     println(calFuelPart1(readTextFileToIntList(pathFile)))
     println(calFuelPart2(readTextFileToIntList(pathFile)))
